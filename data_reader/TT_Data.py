e7ae630c66814edaacc91247ba2a4c957fa58f64
jonkb <jonathan.knight.black@gmail.com>
2017-06-12 15:37:39 -0500

    Finish TT_Data.py

--- data_reader/TT_Data.py
+++ data_reader/TT_Data.py
@@ -46,84 +46,26 @@ def genReport(log):
 			tenSIntervals += 1
 	#Here, do things with the numbers I've collected.
 	#Return something or save something to file. 
+	res = {"tics":tics, "tenSecInts":tenSecInts}
+	res["rewards"] = rewards
+	res["longestInt"] = longestInt
+	res["length"] = time1 - time0
+	res["ticTimes"] = ticTimes
+	res["rewardTimes"] = rewardTimes
+	
 	#print("tics: "+str(tics))
-	return
+	return res
 
+reports = {}
 #Load and process the files
 for i in range(1, len(argv)):
 	file = open(argv[i])
-	genReport(file.read())
-
-#Actually, maybe we'll just use awk from terminal or make a shell script.
-""" FROM TICTRAINER (node.js)
-/**Generates a report for the end of a session
-	Takes the text of the session file (data)
-*/
-function genReport(data){
-	var tics = 0, tenSIntervals = 0, longestInterval = 0;
-	var initL = 0, initP = 0, initT;
-	var endL, endP, endT;
-	var lastTic;
-	var entries = data.split("\n");
-	for(var i = 0; i<entries.length; i++){
-		if(entries[i].trim() == ""){
-			//Ignore blank lines
-			continue;
-		}
-		var entryParts = entries[i].split("|");
-		switch(entryParts[0]){
-			case "session started":
-				initT = new Date(entryParts[1]);
-				lastTic = initT;
-			break;
-			case "starting user l,p,c":
-				initL = entryParts[1].split(",")[0];
-				initP = entryParts[1].split(",")[1];
-			break;
-			case "tic detected":
-				tics++;
-				var ticTime = new Date(entryParts[1]);
-				var ticFree = ticTime - lastTic;
-				lastTic = ticTime;
-				if(ticFree > longestInterval)
-					longestInterval = ticFree;
-				/*Convert ticFree time to seconds. 
-					Divide that by 10s and add that many to the 10s Interval count.
-				*/
-				tenSIntervals += Math.floor(ticFree / 1e4);
-			break;
-			case "session ended":
-				endT = new Date(entryParts[1]);
-			break;
-			case "user l,p,c":
-				endL = entryParts[1].split(",")[0];
-				endP = entryParts[1].split(",")[1];
-			break;
-			default:
-				return "Error: unknown entry: "+entries[i];
-			break;
-		}
-	}
-	if(endL > initL){
-		/*At each levelUp(), points are subtracted
-			and converted to coins.
-			Add those subtracted points to the point total.
-		*/
-		for(var i = initL; i<endL; i++){
-			endP += 1000*i*i;//1000L^2 = nextLevel
-		}
-	}
-	if(tics == 0){
-		//If there were no tics, just use the session length
-		longestInterval = endT - initT;
-	}
-	debugShout("start: "+initT+". end: "+endT);
-	var report = "\n****************\nReport:";
-	report += "\nsession length|"+ (endT - initT);
-	report += "\nlevels gained|"+ (endL - initL);
-	report += "\npoints earned|"+ (endP - initP);
-	report += "\nnumber of tics|"+ tics;
-	report += "\nlongest tic free interval|"+ longestInterval;
-	report += "\nnumber of 10s tic free intervals|"+ tenSIntervals;
-	return report;
-}"""
+	reports[argv[i]] = genReport(file.read())
+#display each
+for f in reports:
+	print("file: "+f)
+	print("session length: "+str(reports[f]["length"]))
+	print("tics: "+str(reports[f]["tics"]))
+	print("ten second tic-free intervals: "+str(reports[f]["tenSecInts"]))
+	print("rewards dispensed: "+str(reports[f]["rewards"]))
+	print("")
